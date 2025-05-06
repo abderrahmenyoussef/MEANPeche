ba4dd7aabe0e5528224b3eba3ca0a1be6f17cf95
abderrahmenyoussef <abderrahmen.youssef@polytechnicien.tn>
2025-05-06 14:29:22 +0100

    feat: set up database connection and configuration

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const databse = require('./src/database/db.config');
 require('dotenv').config();
 const app=express();
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+databse.mongoose.connect(databse.url,{
+}
+).then(()=>{
+    console.log('Connected to the database');
+}
+).catch((err)=>{
+    console.log(err);
+}
+);    
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 }
---
+++ backend/src/config/config.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+const config = {
+    DB_URL: process.env.DATABASE_URL,}
+module.exports = config;
