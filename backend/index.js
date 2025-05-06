c18ea531bc6e38cb20a4d2ffb0dc2c812a6e5d41
abderrahmenyoussef <abderrahmen.youssef@polytechnicien.tn>
2025-05-06 14:17:16 +0100

    chore: initialize backend project with package.json and dependencies

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+}
+);
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+}
+);
